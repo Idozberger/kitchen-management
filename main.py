cc1a75ea59df1cb3ba8ae39f18a295a03a76a9fd
ShahbazM-Dev <shahbaz.devv@gmail.com>
2026-02-10 22:44:04 +0500

    fix

--- main.py
+++ main.py
@@ -1053,7 +1053,7 @@ def swagger_json():
                         'in': 'formData',
                         'type': 'boolean',
                         'required': False,
-                        'default': True,
+                        'default': False,
                         'description': (
                             'Select scanning mode. '
                             'true = Google Document AI + OpenAI enhancement, '
